69c17d8bde873300cc34b695183c3c3de00687de
Greensky <david.hesliere@gmail.com>
2022-12-08 16:29:16 +0100

    perf: :goal_net: Prevent module checking from DM usage

--- src/preconditions/moduleEnabled.ts
+++ src/preconditions/moduleEnabled.ts
@@ -3,6 +3,14 @@ import { Module } from "../utils/functions";
 
 export default new Precondition('moduleEnabled')
     .setChatInputRun(({ command, interaction }) => {
+        if (!interaction.guild) {
+            return {
+                ok: true,
+                interaction,
+                isChatInput: true
+            }
+        }
+
         const state = interaction.client.modulesManager.enabled(interaction.guildId, Module(command.options.name as any))
         if (!state) {
             return {
