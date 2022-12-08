2581eed72f6489754244164c37d96f67e8e78e39
Greensky <david.hesliere@gmail.com>
2022-12-08 16:35:25 +0100

    feat(cmd): :sparkles: Add avatar command

---
+++ src/commands/avatar.ts
@@ -0,0 +1,33 @@
+import { AmethystCommand } from "amethystjs";
+import { ApplicationCommandOptionType } from "discord.js";
+import moduleEnabled from "../preconditions/moduleEnabled";
+
+type allowedImageSizes = 2048 | 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 4096
+const sizes = [256, 512, 1024, 2048, 4096]
+
+export default new AmethystCommand({
+    name: 'avatar',
+    description: "Affiche l'avatar d'un membre du serveur",
+    preconditions: [moduleEnabled],
+    options: [
+        {
+            name: "membre",
+            description: "Personne dont vous voulez voir la photo de profil",
+            type: ApplicationCommandOptionType.User,
+            required: false
+        },
+        {
+            name: "taille",
+            description: "Taille souhaitée de l'image",
+            type: ApplicationCommandOptionType.Integer,
+            required: false,
+            choices: sizes.map((size) => ({ name: `${size}x${size}`, value: size }))
+        }
+    ]
+}).setChatInputRun(({ interaction, options }) => {
+    const user = options.getUser('membre') ?? interaction.user;
+    const size = (options.getInteger('taille', false) ?? 2048) as allowedImageSizes;
+
+    const avatar = user.displayAvatarURL({ size, forceStatic: false });
+    interaction.reply(`Voici l'avatar de ${user.username} :\n${avatar}`).catch(() => {})
+})
