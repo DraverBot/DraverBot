3b9dc6c4e412bde7909ac1b7baad967413cb1941
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-12-08 21:42:56 +0100

    chore: :sparkles: Add silent feature in command denied

--- src/events/commandDenied.ts
+++ src/events/commandDenied.ts
@@ -4,6 +4,8 @@ import replies, { replyKey } from '../data/replies';
 import { systemReply } from '../utils/toolbox';
 
 export default new AmethystEvent('commandDenied', (command, reason) => {
+    if (reason.metadata?.silent === true) return;
+    
     const integrated: { key: commandDeniedCode; value: replyKey }[] = [
         { key: commandDeniedCode.GuildOnly, value: 'guildOnly' },
         { key: commandDeniedCode.DMOnly, value: 'DMOnly' },
