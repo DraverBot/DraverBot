bebdb59f4b52a51e3f0ef8f44e61856288ab3bf9
Greensky <david.hesliere@gmail.com>
2022-12-09 18:40:30 +0100

    chore: :bento: Add paginator buttons

--- src/data/buttons.ts
+++ src/data/buttons.ts
@@ -1,3 +1,4 @@
+import { util } from "../utils/functions"
 import { buildButton, row } from "../utils/toolbox"
 
 export const yesBtn = () => {
@@ -14,6 +15,61 @@ export const noBtn = () => {
         style: 'Danger'
     })
 }
+enum paginatorButtons {
+    Next = "▶️",
+    Previous = "◀️",
+    Last = "⏭️",
+    First = "⏮️",
+    Select = "🔢",
+    Cancel = "❌"
+}
+export const nextPage = (disabled = false) => {
+    return buildButton({
+        emoji: paginatorButtons.Next,
+        id: util('paginatorNext'),
+        style: 'Primary',
+        disabled
+    })
+}
+export const previousPage = (disabled = false) => {
+    return buildButton({
+        emoji: paginatorButtons.Previous,
+        id: util('paginatorPrevious'),
+        style: 'Primary',
+        disabled
+    })
+}
+export const selectButton = () => {
+    return buildButton({
+        emoji: paginatorButtons.Select,
+        id: util('paginatorSelect'),
+        style: 'Success'
+    })
+}
+export const firstPage = (disabled = false) => {
+    return buildButton({
+        emoji: paginatorButtons.First,
+        id: util('paginatorFirst'),
+        style: 'Primary',
+        disabled
+    })
+}
+export const lastPage = (disabled = false) => {
+    return buildButton({
+        emoji: paginatorButtons.Last,
+        id: util('paginatorLast'),
+        style: 'Primary',
+        disabled
+    })
+}
+export const closePaginator = () => {
+    return buildButton({
+        emoji: paginatorButtons.Cancel,
+        id: util('paginatorClose'),
+        style: 'Danger'
+    })
+}
+
 export const yesNoRow = () => {
     return row(yesBtn(), noBtn());
 }
