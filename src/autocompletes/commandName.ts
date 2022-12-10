e303f69baebb7e7fe522011f557695affe985d11
Greensky <david.hesliere@gmail.com>
2022-12-10 11:58:02 +0100

    feat(cmd): :zap: Add command info in help command

---
+++ src/autocompletes/commandName.ts
@@ -0,0 +1,12 @@
+import { AutocompleteListener } from "amethystjs";
+import { compareTwoStrings } from "string-similarity";
+
+export default new AutocompleteListener({
+    listenerName: 'commandName',
+    commandName: [{ commandName: 'help' }],
+    run: ({ interaction, focusedValue }) => {
+        const commands = interaction.client.chatInputCommands.map(x => x.options);
+
+        return commands.filter(x => x.name.includes(focusedValue.toLowerCase()) || focusedValue.toLowerCase().includes(x.name)).splice(0, 24).map((x) => ({ name: x.name, value: x.name }));
+    }
+})
--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,21 +1,64 @@
-import { AmethystCommand } from "amethystjs";
-import { ComponentType, GuildMember, Message, StringSelectMenuBuilder } from "discord.js";
+import { AmethystCommand, preconditions } from "amethystjs";
+import { ApplicationCommandOptionType, ComponentType, GuildMember, Message, StringSelectMenuBuilder } from "discord.js";
 import { modulesData } from "../data/modulesData";
 import replies from "../data/replies";
 import moduleEnabled from "../preconditions/moduleEnabled";
 import { moduleType } from "../typings/database";
-import { commandName } from "../typings/functions";
-import { Module } from "../utils/functions";
-import { basicEmbed, capitalize, row } from "../utils/toolbox";
+import { commandName, permType } from "../typings/functions";
+import { getPerm, Module } from "../utils/functions";
+import { basicEmbed, boolEmoji, capitalize, row } from "../utils/toolbox";
 import { moduleEnabled as moduleEnabledButton } from '../data/buttons';
 
 export default new AmethystCommand({
     name: 'help',
     description: "Affiche la page d'aide des commandes",
     preconditions: [moduleEnabled],
-}).setChatInputRun(async({ interaction }) => {
+    options: [
+        {
+            name: 'commande',
+            description: "Commande que vous voulez voir",
+            type: ApplicationCommandOptionType.String,
+            required: false,
+            autocomplete: true
+        }
+    ]
+}).setChatInputRun(async({ interaction, options }) => {
     const commands: (AmethystCommand & { module: moduleType })[] = interaction.client.chatInputCommands.map((x) => (Object.assign(x, { module: Module(x.options.name as commandName) })));
 
+    const command = options.getString('commande');
+    if (command) {
+        const cmd = commands.find(x => x.options.name === command) as AmethystCommand & { module: moduleType };
+        const subs = cmd.options.options.filter(x => x.type === ApplicationCommandOptionType.Subcommand || x.type === ApplicationCommandOptionType.SubcommandGroup);
+
+        return interaction.reply({
+            embeds: [ basicEmbed(interaction.user, { defaultColor: true })
+                .setTitle(`Commande ${cmd.options.name}`)
+                .setDescription(`${cmd.options.description}`)
+                .setFields(
+                    {
+                        name: "Cooldown",
+                        value: `${cmd.options.cooldown ?? interaction.client.configs.defaultCooldownTime} secondes`,
+                        inline: true
+                    },
+                    {
+                        name: "Utilisable en messages privés",
+                        value: boolEmoji(!cmd.options.preconditions.includes(preconditions.GuildOnly)),
+                        inline: true
+                    },
+                    {
+                        name: "Permissions",
+                        value: cmd.options.permissions?.map(perm => getPerm(perm as permType)).join(' ') ?? 'Pas de permissions',
+                        inline: false
+                    },
+                    {
+                        name: 'Sous-commandes',
+                        value: subs.length === 0 ? "Pas de sous-commandes" : subs.map(x => x.name).join(', '),
+                        inline: true
+                    }
+                )
+            ]
+        })
+    }
     const selector = new StringSelectMenuBuilder()
         .setCustomId('helpSelector')
         .setOptions(
