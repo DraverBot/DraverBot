48537d1054c40360757403db1ba722815f8d17d4
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-12-10 21:04:29 +0100

    chore(precondition): :egg: Add auto-delete precondition

---
+++ src/preconditions/secret.ts
@@ -0,0 +1,10 @@
+import { Precondition } from "amethystjs";
+
+export default new Precondition('secret').setMessageRun(({ message }) => {
+    if (message.deletable) message.delete().catch(() => {});
+    return {
+        ok: true,
+        isChatInput: false,
+        channelMessage: message
+    }
+})
