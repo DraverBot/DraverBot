f064b564ecc6ce653999816f8f149fddad04c8ba
Greensky <david.hesliere@gmail.com>
2022-12-10 12:09:13 +0100

    feat(deps): :heavy_plus_sign: Add interserver-manager

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,6 +3,7 @@ import { CoinsManager } from 'coins-manager';
 import { ModulesManager } from '../managers/modulesManager';
 import { checkDatabase } from '../utils/functions';
 import { database } from '../utils/query';
+import { InterserverManager } from 'interserver-manager';
 
 export default new AmethystEvent('ready', async (client) => {
     await checkDatabase();
@@ -12,14 +13,17 @@ export default new AmethystEvent('ready', async (client) => {
     client.coinsManager = new CoinsManager(database, {
         type: 'multiguild'
     })
+    client.interserver = new InterserverManager(client, database);
 
     // Start managers
     client.coinsManager.start();
+    client.interserver.start();
 });
 
 declare module 'discord.js' {
     interface Client {
         modulesManager: ModulesManager;
         coinsManager: CoinsManager<'multiguild'>;
+        interserver: InterserverManager;
     }
 }
