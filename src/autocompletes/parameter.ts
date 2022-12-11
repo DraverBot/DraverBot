ceb53c348413b42f670c7c0b882df02a95244c7b
Greensky <david.hesliere@gmail.com>
2022-12-11 18:33:20 +0100

    feat: :sparkles: Add parameter autocomplete

---
+++ src/autocompletes/parameter.ts
@@ -0,0 +1,13 @@
+import { AutocompleteListener } from "amethystjs";
+import { configKeys, configsData } from "../data/configData";
+import { capitalize } from "../utils/toolbox";
+
+export default new AutocompleteListener({
+    listenerName: 'paramètre',
+    commandName: [{ commandName: 'configurer', optionName: 'paramètre' }],
+    run: ({ focusedValue }) => {
+        const parameters = Object.keys(configsData).map((x: keyof configKeys) => configsData[x]);
+
+        return parameters.filter(x => x.name.toLowerCase().includes(focusedValue.toLowerCase()) || focusedValue.toLowerCase().includes(x.name.toLowerCase()) || x.description.includes(focusedValue.toLowerCase())).map(x => ({ name: capitalize(x.name), value: x.value }));
+    }
+})
