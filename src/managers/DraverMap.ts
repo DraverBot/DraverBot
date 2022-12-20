35082370a6788085aaf6c4be30323f03794f444c
Greensky <david.hesliere@gmail.com>
2022-12-20 15:04:07 +0100

    ci: :children_crossing: Add a custom draver map for better UX

---
+++ src/managers/DraverMap.ts
@@ -0,0 +1,69 @@
+type MapElement<Key, Value> = { key: Key, value: Value; lastSet: Date };
+
+export class DraverMap<Key = any, Value = any> {
+    private idleTime: number = 60 * 60 * 1000;
+    private datas: MapElement<Key, Value>[] = [];
+
+    constructor(options?: { idleTime?: number }) {
+        this.idleTime = options?.idleTime ?? this.idleTime;
+
+        this.start();
+    }
+
+    private index(key: Key): number {
+        return this.datas.indexOf(this.datas.find(x => x.key === key));
+    }
+    private edit(key: Key, value: Value): this {
+        if (!this.has(key)) return this;
+
+        const index = this.index(key);
+        this.datas[index] = this.objectData(key, value);
+
+        return this;
+    }
+    private objectData(key: Key, value: Value): MapElement<Key, Value> {
+        return {
+            key,
+            value,
+            lastSet: new Date()
+        }
+    }
+    public get(key: Key): Value | undefined {
+        return this.datas.find(x => x.key === key)?.value;
+    }
+    public has(key: Key): boolean {
+        if (this.datas.find(x => x.key === key)) return true;
+        return false;
+    }
+    public delete(key: Key): boolean {
+        const has = this.has(key);
+        this.datas = this.datas.filter(x => x.key !== key);
+
+        return has;
+    }
+    public set(key: Key, value: Value): this {
+        if (!this.has(key)) {
+            this.datas.push(this.objectData(key, value));
+        } else {
+            this.edit(key, value);
+        };
+
+        const now = Date.now() + this.idleTime;
+
+        setTimeout(() => {
+            if (Math.floor(now / 1000) !== Math.floor(Date.now() / 1000)) return;
+
+            this.delete(key);
+        }, this.idleTime);
+
+        return this;
+    }
+
+    public get entries() {
+        return this.datas.map(x => ({ key: x.key, value: x.value }));
+    }
+
+    private start() {
+
+    }
+}
--- src/maps/errors.ts
+++ src/maps/errors.ts
@@ -1,1 +1,3 @@
-export default new Map<string, number>();
+import { DraverMap } from "../managers/DraverMap";
+
+export default new DraverMap<string, number>({ idleTime: 60 * 60 * 1000 });
