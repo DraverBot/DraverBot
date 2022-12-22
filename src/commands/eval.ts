a9eb27dbd246b4087d828de20d7158eccacdc3d9
Greensky <david.hesliere@gmail.com>
2022-12-22 15:03:18 +0100

    feat(tool): :hammer: Add eval command

---
+++ src/commands/eval.ts
@@ -0,0 +1,21 @@
+import { AmethystCommand } from "amethystjs";
+import dev from "../preconditions/dev";
+import { codeBox } from "../utils/toolbox";
+
+export default new AmethystCommand({
+    name: 'eval',
+    preconditions: [dev],
+    description: "Eval"
+}).setMessageRun(async({ message, options }) => {
+    if (options.emptyArgs) return;
+    const run = new Promise(resolve => {
+        resolve(eval(options.args.join(' ')))
+    });
+
+    run.catch((error) => {
+        message.reply(codeBox(error, 'js'));
+    });
+    run.then(result => {
+        if (result) message.reply(codeBox(result as string, 'js'))
+    })
+})
