ee8ea67383733bb4b4f11649a99c1fd2d5680f33
Greensky <david.hesliere@gmail.com>
2022-12-25 11:58:28 +0100

    feat(cmd): :sparkles: Make daily & weekly commands

---
+++ src/commands/daily.ts
@@ -0,0 +1,59 @@
+import { AmethystCommand, preconditions } from 'amethystjs';
+import { util } from '../utils/functions';
+import moduleEnabled from '../preconditions/moduleEnabled';
+import { basicEmbed, evokerColor, numerize, random, secondsToWeeks } from '../utils/toolbox';
+
+export default new AmethystCommand({
+    name: 'quotidien',
+    description: 'Récupère votre récompense de ' + util('coins') + ' quotidienne',
+    preconditions: [preconditions.GuildOnly, moduleEnabled]
+}).setChatInputRun(async ({ interaction }) => {
+    if (
+        interaction.client.cooldownsManager.has({
+            guild_id: interaction.guild.id,
+            user_id: interaction.user.id,
+            commandName: 'quotidien'
+        })
+    )
+        return interaction.reply({
+            embeds: [
+                basicEmbed(interaction.user)
+                    .setTitle('Cooldown')
+                    .setDescription(
+                        `Vous avez déjà utilisé cette commande pendant les 24 dernières heures.\nMerci de patienter encore **${secondsToWeeks(
+                            Math.floor(
+                                interaction.client.cooldownsManager.getRemainingTime({
+                                    guild_id: interaction.guild.id,
+                                    user_id: interaction.user.id,
+                                    commandName: 'quotidien'
+                                }) / 1000
+                            )
+                        )}**`
+                    )
+                    .setColor(evokerColor(interaction.guild))
+            ],
+            ephemeral: true
+        });
+    const coins = random({ max: 8000, min: 1500 });
+
+    interaction.client.coinsManager.addCoins({
+        guild_id: interaction.guild.id,
+        user_id: interaction.user.id,
+        coins
+    });
+    interaction
+        .reply({
+            embeds: [
+                basicEmbed(interaction.user, { defaultColor: true })
+                    .setTitle('Récompense quotidienne')
+                    .setDescription(`Vous avez récupéré **${numerize(coins)} ${util('coins')}** aujourd'hui`)
+            ]
+        })
+        .catch(() => {});
+    interaction.client.cooldownsManager.set({
+        guild_id: interaction.guild.id,
+        user_id: interaction.user.id,
+        time: 86400000,
+        cmd: 'quotidien'
+    });
+});
---
+++ src/commands/weekly.ts
@@ -0,0 +1,59 @@
+import { AmethystCommand, preconditions } from 'amethystjs';
+import { util } from '../utils/functions';
+import moduleEnabled from '../preconditions/moduleEnabled';
+import { basicEmbed, evokerColor, numerize, random, secondsToWeeks } from '../utils/toolbox';
+
+export default new AmethystCommand({
+    name: 'hebdomadaire',
+    description: 'Récupère votre récompense de ' + util('coins') + ' hebdomadaire',
+    preconditions: [preconditions.GuildOnly, moduleEnabled]
+}).setChatInputRun(async ({ interaction }) => {
+    if (
+        interaction.client.cooldownsManager.has({
+            guild_id: interaction.guild.id,
+            user_id: interaction.user.id,
+            commandName: 'hebdomadaire'
+        })
+    )
+        return interaction.reply({
+            embeds: [
+                basicEmbed(interaction.user)
+                    .setTitle('Cooldown')
+                    .setDescription(
+                        `Vous avez déjà utilisé cette commande pendant les 7 derniers jours.\nMerci de patienter encore **${secondsToWeeks(
+                            Math.floor(
+                                interaction.client.cooldownsManager.getRemainingTime({
+                                    guild_id: interaction.guild.id,
+                                    user_id: interaction.user.id,
+                                    commandName: 'hebdomadaire'
+                                }) / 1000
+                            )
+                        )}**`
+                    )
+                    .setColor(evokerColor(interaction.guild))
+            ],
+            ephemeral: true
+        });
+    const coins = random({ max: 11391, min: 7000 });
+
+    interaction.client.coinsManager.addCoins({
+        guild_id: interaction.guild.id,
+        user_id: interaction.user.id,
+        coins
+    });
+    interaction
+        .reply({
+            embeds: [
+                basicEmbed(interaction.user, { defaultColor: true })
+                    .setTitle('Récompense hebdomadaire')
+                    .setDescription(`Vous avez récupéré **${numerize(coins)} ${util('coins')}** cette semaine`)
+            ]
+        })
+        .catch(() => {});
+    interaction.client.cooldownsManager.set({
+        guild_id: interaction.guild.id,
+        user_id: interaction.user.id,
+        time: 604800000,
+        cmd: 'hebdomadaire'
+    });
+});
