4bbfd55875069a8be85bb2e91fce50247a080eee
Greensky <david.hesliere@gmail.com>
2022-12-31 18:52:10 +0100

    chore(tool): :goal_net: Add webhook send msg on query catch

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -1,6 +1,9 @@
 import { createConnection } from 'mysql';
 import { config } from 'dotenv';
 import { DefaultQueryResult, QueryResult } from '../typings/database';
+import { EmbedBuilder, WebhookClient } from 'discord.js';
+import { util } from './functions';
+import { codeBox } from './toolbox';
 config();
 
 export const database = createConnection(process.env);
@@ -14,7 +17,26 @@ database.connect((error) => {
 export default function <T = DefaultQueryResult>(query: string): Promise<QueryResult<T>> {
     return new Promise((resolve, reject) => {
         database.query(query, (error, request) => {
-            if (error) return reject(error);
+            if (error) {
+                reject(error);
+                const webhook = new WebhookClient({
+                    url: util('errorWebhook')
+                });
+
+                if (webhook) {
+                    webhook
+                        .send({
+                            embeds: [
+                                new EmbedBuilder({
+                                    title: 'Erreur SQL',
+                                    description: `Requpête: ${codeBox(query, 'sql')}\nErreur: ${codeBox(query, 'sql')}`,
+                                    timestamp: new Date(Date.now())
+                                })
+                            ]
+                        })
+                        .catch(() => {});
+                }
+            }
             resolve(request);
         });
     });
