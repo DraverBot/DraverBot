9d372161ab73eec54f3728cb71888c27f59912bd
Greensky <david.hesliere@gmail.com>
2023-01-03 17:23:31 +0100

    fix(cmd): :lock: Fix nsfw issues in google command

--- src/commands/google.ts
+++ src/commands/google.ts
@@ -1,7 +1,9 @@
 import { AmethystCommand } from 'amethystjs';
-import { ApplicationCommandOptionType } from 'discord.js';
+import { ApplicationCommandOptionType, TextChannel } from 'discord.js';
 import moduleEnabled from '../preconditions/moduleEnabled';
-import { basicEmbed } from '../utils/toolbox';
+import { basicEmbed, evokerColor, hint } from '../utils/toolbox';
+import NSFWwords from '../data/NSFWwords.json';
+import { util } from '../utils/functions';
 
 export default new AmethystCommand({
     name: 'google',
@@ -16,6 +18,24 @@ export default new AmethystCommand({
     ],
     preconditions: [moduleEnabled]
 }).setChatInputRun(async ({ interaction, options }) => {
+    const search = options.getString('recherche');
+    if (NSFWwords.some((x) => search.toLowerCase().includes(x)) && !(interaction.channel as TextChannel).nsfw) {
+        return interaction.reply({
+            embeds: [
+                basicEmbed(interaction.user)
+                    .setTitle('Contenu NSFW')
+                    .setDescription(
+                        `Il semblerait que votre recherche contiennent du contenu NSFW.\nVeillez à utiliser cette commande dans un salon NSFW.\n${hint(
+                            `Si vous pensez que ce message est une erreur, faites-nous part de cette expérience sur [le serveur de support](${util(
+                                'support'
+                            )})`
+                        )}`
+                    )
+                    .setColor(evokerColor(interaction.guild))
+            ]
+        });
+    }
+
     interaction
         .reply({
             embeds: [
