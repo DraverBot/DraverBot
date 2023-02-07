e3cab84f06d16179a90a55d09b073a7c1d0a41cc
Greensky <david.hesliere@gmail.com>
2023-02-07 16:54:12 +0100

    chore(tool): :hammer: Add removeKey function

--- src/utils/toolbox.ts
+++ src/utils/toolbox.ts
@@ -598,3 +598,7 @@ export const sendError = (error: unknown) => {
         ]
     }).catch(() => {});
 };
+export const removeKey = <T, K extends keyof T>(obj: T, key: K): Omit<T, K> => {
+    const { [key]: _, ...rest } = obj;
+    return rest;
+};
