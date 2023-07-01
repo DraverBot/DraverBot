86976b6ababd5479644c40bd483e5c090526590e
Greensky-gs <david.hesliere@gmail.com>
2023-07-01 11:41:21 +0200

    chore: :fire: Remove bender api post process

--- src/process/BenderApi.ts
+++
@@ -1,8 +0,0 @@
-import { Process } from '../structures/Process';
-import { modActionType } from '../typings/database';
-import { BenderAPIOptions } from '../typings/apis';
-
-export default new Process(
-    'Bender API Post',
-    async <T extends keyof typeof modActionType>(type: T, {}: BenderAPIOptions<T>) => {}
-);
